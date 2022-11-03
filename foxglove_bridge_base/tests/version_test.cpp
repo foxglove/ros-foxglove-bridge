fa2164c140b0ab90eeffc94417edd4b0a58a9a2a
John Hurliman <jhurliman@jhurliman.org>
2022-11-03 12:06:48 -0700

    Add CI checks (#4)
    **Public-Facing Changes**
    
    **Description**
    Adds CI checks for linting and testing across various ROS 1 and ROS 2 releases

---
+++ foxglove_bridge_base/tests/version_test.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include <foxglove_bridge/foxglove_bridge.hpp>
+
+TEST(VersionTest, TestWebSocketVersion) {
+  // ex: "WebSocket++/0.8.1"
+  const std::string version = foxglove::WebSocketUserAgent();
+  EXPECT_EQ(version.substr(0, 14), "WebSocket++/0.");
+}
