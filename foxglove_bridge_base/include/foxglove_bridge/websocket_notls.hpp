e57d878df9439aaa0b5b002e76f7856746d418fd
John Hurliman <jhurliman@jhurliman.org>
2022-11-28 18:47:24 -0800

    Fix WebSocket log messages sometimes going straight to stdout/stderr

--- foxglove_bridge_base/include/foxglove_bridge/websocket_notls.hpp
+++ foxglove_bridge_base/include/foxglove_bridge/websocket_notls.hpp
@@ -7,11 +7,34 @@
 
 namespace foxglove {
 
-struct WebSocketNoTls : public websocketpp::config::asio {
-public:
-  // Replace default stream loggers with custom loggers
-  typedef CallbackLogger<concurrency_type, websocketpp::log::elevel> elog_type;
-  typedef CallbackLogger<concurrency_type, websocketpp::log::alevel> alog_type;
+struct WebSocketNoTls : public websocketpp::config::core {
+  typedef WebSocketNoTls type;
+  typedef core base;
+
+  typedef base::concurrency_type concurrency_type;
+
+  typedef base::request_type request_type;
+  typedef base::response_type response_type;
+
+  typedef base::message_type message_type;
+  typedef base::con_msg_manager_type con_msg_manager_type;
+  typedef base::endpoint_msg_manager_type endpoint_msg_manager_type;
+
+  typedef CallbackLogger alog_type;
+  typedef CallbackLogger elog_type;
+
+  typedef base::rng_type rng_type;
+
+  struct transport_config : public base::transport_config {
+    typedef type::concurrency_type concurrency_type;
+    typedef CallbackLogger alog_type;
+    typedef CallbackLogger elog_type;
+    typedef type::request_type request_type;
+    typedef type::response_type response_type;
+    typedef websocketpp::transport::asio::basic_socket::endpoint socket_type;
+  };
+
+  typedef websocketpp::transport::asio::endpoint<transport_config> transport_type;
 };
 
 }  // namespace foxglove
