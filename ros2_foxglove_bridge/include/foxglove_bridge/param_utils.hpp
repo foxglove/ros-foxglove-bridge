5b675266dfdd0417ed6ac3a0c20902f84a65e5b6
Hans-Joachim Krauch <achim.krauch@gmail.com>
2022-12-15 20:40:08 +0000

    Move parameter declaration code into separate file

---
+++ ros2_foxglove_bridge/include/foxglove_bridge/param_utils.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <regex>
+#include <string>
+#include <vector>
+
+#include <rclcpp/node.hpp>
+
+namespace foxglove_bridge {
+
+constexpr char PARAM_PORT[] = "port";
+constexpr char PARAM_ADDRESS[] = "address";
+constexpr char PARAM_USETLS[] = "tls";
+constexpr char PARAM_CERTFILE[] = "certfile";
+constexpr char PARAM_KEYFILE[] = "keyfile";
+constexpr char PARAM_MAX_QOS_DEPTH[] = "max_qos_depth";
+constexpr char PARAM_TOPIC_WHITELIST[] = "topic_whitelist";
+
+constexpr int64_t DEFAULT_PORT = 8765;
+constexpr char DEFAULT_ADDRESS[] = "0.0.0.0";
+constexpr int64_t DEFAULT_MAX_QOS_DEPTH = 10;
+
+void declareParameters(rclcpp::Node* node);
+
+std::vector<std::regex> parseRegexStrings(rclcpp::Node* node,
+                                          const std::vector<std::string>& strings);
+
+}  // namespace foxglove_bridge
