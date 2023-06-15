1e277d40c8a2f46f7dc5f718556fa803f5c2f515
Hans-Joachim Krauch <achim-k@users.noreply.github.com>
2023-06-15 16:23:15 +0200

    Update ros2_foxglove_bridge.cpp

--- ros2_foxglove_bridge/src/ros2_foxglove_bridge.cpp
+++ ros2_foxglove_bridge/src/ros2_foxglove_bridge.cpp
@@ -580,11 +580,11 @@ void FoxgloveBridge::clientAdvertise(const foxglove::ClientAdvertisement& advert
                      return endpoint.node_name() != this->get_name() ||
                             endpoint.node_namespace() != this->get_namespace();
                    });
-    const rclcpp::QoS qos = otherPublisherIt == otherPublishers.end()
-                              ? rclcpp::SystemDefaultsQoS()
-                              : rclcpp::QoS(otherPublisherIt->qos_profile())
-                                  // Override the history policy as it can be `unknown`
-                                  .history(rclcpp::HistoryPolicy::SystemDefault);
+    rclcpp::QoS qos = otherPublisherIt == otherPublishers.end() ? rclcpp::SystemDefaultsQoS()
+                                                                : otherPublisherIt->qos_profile();
+    if (qos.history() == rclcpp::HistoryPolicy::Unknown) {
+      qos.history(rclcpp::HistoryPolicy::SystemDefault);
+    }
     rclcpp::PublisherOptions publisherOptions{};
     publisherOptions.callback_group = _clientPublishCallbackGroup;
     auto publisher = this->create_generic_publisher(topicName, topicType, qos, publisherOptions);
