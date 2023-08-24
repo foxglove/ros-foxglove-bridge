935338455f114c15bca1a244b09a9a71826879a6
artivis <jeremie.deray@canonical.com>
2023-08-24 19:34:31 +0200

    add snap packaging
    Signed-off-by: artivis <jeremie.deray@canonical.com>

---
+++ snap/local/launch.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/sh -e
+
+LAUNCH_OPTIONS=""
+
+OPTIONS="port
+address
+tls
+certfile
+keyfile
+topic-whitelist
+param-whitelist
+service-whitelist
+client-topic-whitelist
+min-qos-depth
+max-qos-depth
+num-threads
+send-buffer-limit
+use-sim-time
+capabilities
+include-hidden
+asset-uri-allowlist"
+
+for OPTION in ${OPTIONS}; do
+  VALUE="$(snapctl get ${OPTION})"
+  if [ -n "${VALUE}" ]; then
+    LAUNCH_OPTIONS+="${OPTION}:=${VALUE} "
+  fi
+done
+
+# Replace '-' with '_'
+LAUNCH_OPTIONS=$(echo ${LAUNCH_OPTIONS} | tr - _)
+
+if [ "${LAUNCH_OPTIONS}" ]; then
+  logger -t ${SNAP_NAME} "Running with options: ${LAUNCH_OPTIONS}"
+fi
+
+ros2 launch foxglove_bridge foxglove_bridge_launch.xml ${LAUNCH_OPTIONS}
---
+++ snap/local/reset_config.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+# All parameters are unset
+# leaving the default values up to the launchfile.
+
+snapctl set port!
+snapctl set address!
+snapctl set tls!
+snapctl set certfile!
+snapctl set keyfile!
+snapctl set topic_whitelist!
+snapctl set param_whitelist!
+snapctl set service_whitelist!
+snapctl set client_topic_whitelist!
+snapctl set min_qos_depth!
+snapctl set max_qos_depth!
+snapctl set num_threads!
+snapctl set send_buffer_limit!
+snapctl set use_sim_time!
+snapctl set capabilities!
+snapctl set include_hidden!
+snapctl set asset_uri_allowlist!
