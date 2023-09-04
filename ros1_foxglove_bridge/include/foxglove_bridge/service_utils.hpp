53ad89bc12d4c236aa6343e21bb592a1dbcff8b9
Hans-Joachim Krauch <achim.krauch@gmail.com>
2023-09-04 12:24:05 +0000

    Make service type retrieval more robust

--- ros1_foxglove_bridge/include/foxglove_bridge/service_utils.hpp
+++ ros1_foxglove_bridge/include/foxglove_bridge/service_utils.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <future>
 #include <string>
 
 namespace foxglove_bridge {
@@ -9,6 +8,6 @@ namespace foxglove_bridge {
  * Opens a socket to the service server and retrieves the service type from the connection header.
  * This is necessary as the service type is not stored on the ROS master.
  */
-std::future<std::string> retrieveServiceType(const std::string& serviceName);
+std::string retrieveServiceType(const std::string& serviceName, int timeout_ms);
 
 }  // namespace foxglove_bridge
