9d920682c4b997a1e7546b5ca09f26f9daa3b940
Hans-Joachim Krauch <achim-k@users.noreply.github.com>
2024-02-24 13:10:57 -0300

    Fix rolling builds (#289)
    ### Public-Facing Changes
    None
    
    ### Description
    Fixes ROS rolling compile errors, namely:
    - Replace std::bind wtih lambda to make it compatible with latest
    rolling changes
    - Fix deprecation warning (`get_typesupport_handle` is deprecated on
    rolling)
    
    Fixes #284 (hopefully)
    Resolves FG-6444

--- ros2_foxglove_bridge/src/generic_client.cpp
+++ ros2_foxglove_bridge/src/generic_client.cpp
@@ -9,6 +9,21 @@
 
 #include <foxglove_bridge/generic_client.hpp>
 
+// clang-format off
+/* True if the version of RCLCPP is at least major.minor.patch */
+#define RCLCPP_VERSION_GTE(major, minor, patch)        \
+  (major < RCLCPP_VERSION_MAJOR                        \
+     ? true                                            \
+     : major > RCLCPP_VERSION_MAJOR                    \
+         ? false                                       \
+         : minor < RCLCPP_VERSION_MINOR                \
+             ? true                                    \
+             : minor > RCLCPP_VERSION_MINOR            \
+                 ? false                               \
+                 : patch < RCLCPP_VERSION_PATCH ? true \
+                                                : patch > RCLCPP_VERSION_PATCH ? false : true)
+// clang-format on
+
 namespace {
 
 // Copy of github.com/ros2/rclcpp/blob/33dae5d67/rclcpp/src/rclcpp/typesupport_helpers.cpp#L69-L92
@@ -123,10 +138,19 @@ GenericClient::GenericClient(rclcpp::node_interfaces::NodeBaseInterface* nodeBas
   _typeIntrospectionHdl = (reinterpret_cast<decltype(get_ts)>(
     _typeIntrospectionLib->get_symbol(typeinstrospection_symbol_name)))();
 
+  // get_typesupport_handle is deprecated since rclcpp 25.0.0
+  // (https://github.com/ros2/rclcpp/pull/2209)
+#if RCLCPP_VERSION_GTE(25, 0, 0)
+  _requestTypeSupportHdl =
+    rclcpp::get_message_typesupport_handle(requestTypeName, TYPESUPPORT_LIB_NAME, *_typeSupportLib);
+  _responseTypeSupportHdl = rclcpp::get_message_typesupport_handle(
+    responseTypeName, TYPESUPPORT_LIB_NAME, *_typeSupportLib);
+#else
   _requestTypeSupportHdl =
     rclcpp::get_typesupport_handle(requestTypeName, TYPESUPPORT_LIB_NAME, *_typeSupportLib);
   _responseTypeSupportHdl =
     rclcpp::get_typesupport_handle(responseTypeName, TYPESUPPORT_LIB_NAME, *_typeSupportLib);
+#endif
 
   rcl_ret_t ret = rcl_client_init(this->get_client_handle().get(), this->get_rcl_node_handle(),
                                   _serviceTypeSupportHdl, serviceName.c_str(), &client_options);
