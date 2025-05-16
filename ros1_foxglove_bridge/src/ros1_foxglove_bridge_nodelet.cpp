7036b2655981d871666991c9170cdf15286fb719
Shih-Wei Guo <tony2guo@gmail.com>
2025-05-16 13:39:04 +0800

    Update asset_uri_allowlist regex to allow percent-encoding

--- ros1_foxglove_bridge/src/ros1_foxglove_bridge_nodelet.cpp
+++ ros1_foxglove_bridge/src/ros1_foxglove_bridge_nodelet.cpp
@@ -106,8 +106,8 @@ public:
 
     const auto assetUriAllowlist = nhp.param<std::vector<std::string>>(
       "asset_uri_allowlist",
-      {"^package://(?:\\w+/"
-       ")*\\w+\\.(?:dae|fbx|glb|gltf|jpeg|jpg|mtl|obj|png|stl|tif|tiff|urdf|webp|xacro)$"});
+      {"^package://(?:[-\\w%]+/"
+       ")*[-\\w%]+\\.(?:dae|fbx|glb|gltf|jpeg|jpg|mtl|obj|png|stl|tif|tiff|urdf|webp|xacro)$"});
     _assetUriAllowlistPatterns = parseRegexPatterns(assetUriAllowlist);
     if (assetUriAllowlist.size() != _assetUriAllowlistPatterns.size()) {
       ROS_ERROR("Failed to parse one or more asset URI whitelist patterns");
